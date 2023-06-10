34734aa1592ddb0ae5199827bd5884205af9fa5b
MN08 <mnurhuda009@gmail.com>
2023-06-10 20:44:29 +0700

    create getCampaign Handler

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,31 @@
 package handler
+
+import (
+	"letsfunding/campaign"
+	"letsfunding/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.APIResponse("Get Campaign Failed", http.StatusBadRequest, "Error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("Get Campaign Succeed", http.StatusOK, "Success", campaigns)
+	c.JSON(http.StatusOK, response)
+}
