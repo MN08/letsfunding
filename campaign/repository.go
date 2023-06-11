27d49196305bde1657391980dc18d2fca88c68f7
MN08 <mnurhuda009@gmail.com>
2023-06-11 19:49:28 +0700

    add findbyid repository

--- campaign/repository.go
+++ campaign/repository.go
@@ -6,6 +6,7 @@ type (
 	Repository interface {
 		FindAll() ([]Campaign, error)
 		FindByUserID(userID int) ([]Campaign, error)
+		FindByID(ID int) (Campaign, error)
 	}
 
 	repository struct {
@@ -34,4 +35,13 @@ func (r *repository) FindByUserID(userID int) ([]Campaign, error) {
 		return campaigns, err
 	}
 	return campaigns, nil
+}
+
+func (r *repository) FindByID(ID int) (Campaign, error) {
+	var campaign Campaign
+	err := r.db.Preload("User").Where("id = ?", ID).Preload("CampaignImages").Find(&campaign).Error
+	if err != nil {
+		return campaign, err
+	}
+	return campaign, nil
 }
