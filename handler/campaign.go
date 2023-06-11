af2a93327eb281b0a39fd211f24ca708c8452b97
MN08 <mnurhuda009@gmail.com>
2023-06-11 19:50:12 +0700

    add get single campaign

--- handler/campaign.go
+++ handler/campaign.go
@@ -29,3 +29,23 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	response := helper.APIResponse("Get Campaign Succeed", http.StatusOK, "Success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *campaignHandler) GetCampaign(c *gin.Context) {
+	var input campaign.GetCampaignDetailInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign Detail", http.StatusBadRequest, "Error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	campaignDetail, err := h.service.GetCampaignByID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign Detail", http.StatusBadRequest, "Error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("Campaign Detail", http.StatusOK, "Success", campaign.FormatDetailCampaign(campaignDetail))
+	c.JSON(http.StatusOK, response)
+}
